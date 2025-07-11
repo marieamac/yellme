6aa92ef67c24c0bd1358fe9d15bdfe13a8bcf712
marieamac <marieamacarthur.publishing@gmail.com>
2025-07-10 22:49:53 -0400

    Update uploadRecording.js

--- api/uploadRecording.js
+++ api/uploadRecording.js
@@ -1,4 +1,3 @@
-import { Readable } from 'stream';
 import getRawBody from 'raw-body';
 
 export const config = {
@@ -15,25 +14,28 @@ export default async function handler(req, res) {
   try {
     const audioBuffer = await getRawBody(req);
     console.log("🟡 Audio recibido. Tamaño:", audioBuffer.length);
+
     const response = await fetch('https://api.assemblyai.com/v2/upload', {
       method: 'POST',
       headers: {
         'Authorization': '4a57658e552c4206849da79cd79eb24a',
-        'Transfer-Encoding': 'chunked',
+        'Content-Type': 'application/octet-stream'
       },
-      body: Readable.from(audioBuffer),
-      duplex: 'half',
+      body: audioBuffer
     });
 
     const data = await response.json();
 
     if (!response.ok) {
+      console.error("❌ Error desde AssemblyAI:", data);
       return res.status(500).json({ error: 'Fallo en la subida', details: data });
     }
 
+    console.log("✅ Audio subido correctamente:", data);
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: 'Error del servidor', details: error.message });
     console.error("❌ ERROR en uploadRecording:", error);
+    res.status(500).json({ error: 'Error del servidor', details: error.message });
   }
 }
+
